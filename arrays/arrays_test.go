93106b992608f231bc15941937101f132abc63fd
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-12-26 22:44:41 +0100

    👷 Sum array of numbers

---
+++ arrays/arrays_test.go
@@ -0,0 +1,17 @@
+package arrays
+
+import "testing"
+
+func TestSum(t *testing.T) {
+	t.Run("sum of 5 numbers", func(t *testing.T) {
+        numbers := []int{1, 1, 1, 1, 1}
+
+        want := 5
+        got := Sum(numbers)
+
+        // %v placeholder prints the inputs in a readable format
+        if got != want {
+            t.Errorf("got %d want %d given, %v", got, want, numbers)
+        }
+    })
+}
