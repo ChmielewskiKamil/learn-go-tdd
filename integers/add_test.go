f51173c3185d1b240e6e77cde7d8bf0962545aa3
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-12-26 16:37:37 +0100

    👷 Create addition functionality

---
+++ integers/add_test.go
@@ -0,0 +1,23 @@
+package integers
+
+import "testing"
+
+func TestAdder(t *testing.T) {
+	t.Run("2 + 2 = 4", func(t *testing.T) {
+		sum := Add(2, 2)
+		expected := 4
+
+		if sum != expected {
+			t.Errorf("expected '%d' but got '%d'", expected, sum)
+		}
+	})
+
+    t.Run("3 + 5 = 8", func(t *testing.T) {
+        sum := Add(3, 5)
+        expected := 8
+
+        if sum != expected {
+            t.Errorf("expected '%d' but got '%d'", expected, sum)
+        }
+    })
+}
