97deeb7bd312de9597e7a805286ef9a84603e411
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-12-26 22:44:26 +0100

    🎨 Format code

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -1,12 +1,38 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
+	t.Run("repeat 5 times", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
+		if repeated != expected {
+			t.Errorf("expected %q but got %q", expected, repeated)
+		}
+	})
+
+    t.Run("user specify the repeat count", func(t *testing.T) {
+        repeated := Repeat("a", 7)
+        expected := "aaaaaaa"
+
+        if repeated != expected {
+            t.Errorf("expected %q but got %q", expected, repeated)
+        }
+    })
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 5)
+	fmt.Println(repeated)
+	// Output: aaaaa
 }
