cf37089e60e70b6d9073cd41601ae00278caf893
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-12-26 16:57:39 +0100

    👷 Experiment with doc comments

--- integers/add.go
+++ integers/add.go
@@ -1,5 +1,8 @@
+// Integers is a package that handles integer operations
 package integers
 
+// This is an example doc comment
 func Add(x, y int) int {
+    // Will this show as well? No, it won't
     return x + y
 }
