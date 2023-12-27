67aa9329b3c7a519f791cd31e6cd00826d623c2f
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-12-27 22:17:24 +0100

    👷 Handle area of triangles
    ♻️ Improve readability in the tests

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -5,16 +5,17 @@ import "testing"
 func TestArea(t *testing.T) {
 	areaTests := []struct {
 		shape Shape
-		want  float64
+		expectedArea  float64
 	}{
-		{Rectangle{Width: 2.5, Height: 5.0}, 12.5},
-        {Circle{Radius: 5.0}, 78.53981633974483},
+        {shape: Rectangle{Width: 2.5, Height: 5.0}, expectedArea: 12.5},
+        {shape: Circle{Radius: 5.0}, expectedArea: 78.53981633974483},
+        {shape: Triangle{Base: 12, Height: 6}, expectedArea: 36.0},
 	}
     
     for _, tt := range areaTests {
-        got := tt.shape.Area()
-        if got != tt.want {
-            t.Errorf("got %g want %g", got, tt.want)
+        calculatedArea := tt.shape.Area()
+        if calculatedArea != tt.expectedArea {
+            t.Errorf("Calculated area: %g, while it should be: %g", calculatedArea, tt.expectedArea)
         }
     }
 }
