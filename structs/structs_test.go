58ab4be252f47aed76efe42e14b31c6062cb6507
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-12-27 22:03:04 +0100

    ♻️ Introduce Shape interface to simplify tests

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -3,23 +3,23 @@ package structs
 import "testing"
 
 func TestArea(t *testing.T) {
-    t.Run("rectangle area", func(t *testing.T){
-        rectangle := Rectangle{Width: 2.5, Height: 5.0}
-        got := rectangle.Area()
-        want := 12.5
+	checkArea := func(t testing.TB, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
+	}
 
-        if got != want {
-            t.Errorf("got %.2f want %.2f", got, want)
-        }
-    })
+	t.Run("rectangle area", func(t *testing.T) {
+		rectangle := Rectangle{Width: 2.5, Height: 5.0}
+		want := 12.5
+		checkArea(t, rectangle, want)
+	})
 
-    t.Run("circle area", func(t *testing.T) {
-        circle := Circle{Radius: 5.0}
-        got := circle.Area()
-        want := 78.53981633974483
-
-        if got != want {
-            t.Errorf("got %g want %g", got, want)
-        }
-    })
+	t.Run("circle area", func(t *testing.T) {
+		circle := Circle{Radius: 5.0}
+		want := 78.53981633974483
+        checkArea(t, circle, want)
+	})
 }
