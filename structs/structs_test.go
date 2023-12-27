2426ee03f89f6e7e3976433e38db9960325f0129
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-12-27 22:27:32 +0100

    ♻️ Use test name to improve readability
    🐛 Fix triangle bug

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -4,18 +4,21 @@ import "testing"
 
 func TestArea(t *testing.T) {
 	areaTests := []struct {
-		shape Shape
-		expectedArea  float64
+		testName     string
+		shape        Shape
+		expectedArea float64
 	}{
-        {shape: Rectangle{Width: 2.5, Height: 5.0}, expectedArea: 12.5},
-        {shape: Circle{Radius: 5.0}, expectedArea: 78.53981633974483},
-        {shape: Triangle{Base: 12, Height: 6}, expectedArea: 36.0},
+		{testName: "Rectangle", shape: Rectangle{Width: 2.5, Height: 5.0}, expectedArea: 12.5},
+		{testName: "Circle", shape: Circle{Radius: 5.0}, expectedArea: 78.53981633974483},
+		{testName: "Triangle", shape: Triangle{Base: 12, Height: 6}, expectedArea: 36.0},
 	}
-    
-    for _, tt := range areaTests {
-        calculatedArea := tt.shape.Area()
-        if calculatedArea != tt.expectedArea {
-            t.Errorf("Calculated area: %g, while it should be: %g", calculatedArea, tt.expectedArea)
-        }
-    }
+
+	for _, tt := range areaTests {
+		t.Run(tt.testName, func(t *testing.T) {
+			calculatedArea := tt.shape.Area()
+			if calculatedArea != tt.expectedArea {
+				t.Errorf("Calculated area: %g, while it should be: %g", calculatedArea, tt.expectedArea)
+			}
+		})
+	}
 }
