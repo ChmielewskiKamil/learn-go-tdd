ed4d80d739d174aa17779d8e0c8bec7d52e8562b
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-12-27 21:15:08 +0100

    👷 Calculate circle Area

--- structs/structs_test.go
+++ structs/structs_test.go
@@ -4,11 +4,22 @@ import "testing"
 
 func TestArea(t *testing.T) {
     t.Run("rectangle area", func(t *testing.T){
-        got := Area(2.5, 5.0)
+        rectangle := Rectangle{Width: 2.5, Height: 5.0}
+        got := rectangle.Area()
         want := 12.5
 
         if got != want {
             t.Errorf("got %.2f want %.2f", got, want)
+        }
+    })
+
+    t.Run("circle area", func(t *testing.T) {
+        circle := Circle{Radius: 5.0}
+        got := circle.Area()
+        want := 78.53981633974483
+
+        if got != want {
+            t.Errorf("got %g want %g", got, want)
         }
     })
 }
