3898aed9c3707f758162c7e8dc483f6c5d954adc
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-12-28 16:26:36 +0100

    ✨ Add Withdraw functionality
    ♻️ Remove duplicated logic
    
    ♻️ Pass value directly instead of creating new var

--- pointers/pointers_test.go
+++ pointers/pointers_test.go
@@ -3,15 +3,25 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(wallet Wallet, want Bitcoin, t testing.TB) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
 
-		got := wallet.Balance()
-		want := Bitcoin(10)
+		assertBalance(wallet, Bitcoin(10), t)
+	})
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(10))
+
+		assertBalance(wallet, Bitcoin(10), t)
 	})
 }
