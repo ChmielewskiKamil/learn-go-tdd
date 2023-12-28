98c640792bf00f7ed63dc71dafff183c50487b75
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2023-12-28 21:40:27 +0100

    👷 Handle unknown words

--- maps/maps.go
+++ maps/maps.go
@@ -1,7 +1,12 @@
 package maps
 
+import "errors"
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) (entry string) {
-    return d[word]
+func (d Dictionary) Search(word string) (entry string, searchError error) {
+    if d[word] == "" {
+        return "", errors.New("Sorry, the word you are looking for is not in the dictionary.")
+    }
+    return d[word], nil 
 }
--- maps/maps_test.go
+++ maps/maps_test.go
@@ -6,11 +6,24 @@ func TestDictionarySearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
 	t.Run("known word", func(t *testing.T) {
-		got := dictionary.Search("test")
+		got, _ := dictionary.Search("test")
 		want := "this is just a test"
 
 		if got != want {
 			t.Errorf("got %q want %q", got, want)
 		}
 	})
+
+    t.Run("unknown word", func(t *testing.T) {
+        _, err := dictionary.Search("unknown")
+        want := "Sorry, the word you are looking for is not in the dictionary."
+
+        if err == nil {
+            t.Fatal("expected to get an error, but didn't get one")
+        }
+
+        if err.Error() != want {
+            t.Errorf("got %q want %q", err, want)
+        }
+    })
 }
