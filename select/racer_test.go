da64c8d080800848d0590c4080f36802eb9c0747
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-01-02 16:00:17 +0100

    ♻️ Use http test from std lib

--- select/racer_test.go
+++ select/racer_test.go
@@ -1,15 +1,30 @@
 package racer
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+)
 
 func TestRacer(t *testing.T) {
-    slowURL := "http://www.facebook.com"
-    fastURL := "http://www.quii.dev"
+	slowServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(20 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
 
-    want := fastURL
-    got := Racer(slowURL, fastURL)
+	fastServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		time.Sleep(10 * time.Millisecond)
+		w.WriteHeader(http.StatusOK)
+	}))
 
-    if got != want {
-        t.Errorf("got %q, want %q", got, want)
-    }
+	slowURL := slowServer.URL
+	fastURL := fastServer.URL
+
+	want := fastURL
+	got := Racer(slowURL, fastURL)
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
 }
