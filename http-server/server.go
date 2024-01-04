e3c76bbd2e2fe45b777cada89b9667ec988d37f2
Kamil Chmielewski <chmielewski.kamil45@gmail.com>
2024-01-04 14:05:05 +0100

    👷 Return scores for 2 players
    ♻️ Simplify test suite
    
    ♻️ Extract get score request to separate function

--- http-server/server.go
+++ http-server/server.go
@@ -3,8 +3,22 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
 func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+	player := strings.TrimPrefix(r.URL.String(), "/players/")
+	fmt.Fprint(w, GetPlayerScore(player))
+}
+
+func GetPlayerScore(player string) (score string) {
+	if player == "Bob" {
+		score = "10"
+	}
+
+	if player == "Alice" {
+		score = "20"
+	}
+
+	return
 }
--- http-server/server_test.go
+++ http-server/server_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -8,16 +9,33 @@ import (
 
 func TestGETPlayers(t *testing.T) {
 	t.Run("it gets Alice's score", func(t *testing.T) {
-		request, _ := http.NewRequest("http.MethodGet", "/players/Alice", nil)
+		request := newGetScoreRequest("Alice")
 		response := httptest.NewRecorder()
 
 		PlayerServer(response, request)
 
-		got := response.Body.String()
-		want := "20"
-
-		if got != want {
-			t.Errorf("got: %q, want: %q", got, want)
-		}
+		assertResponseBody(t, response.Body.String(), "20")
 	})
+
+	t.Run("it gets Bob's score", func(t *testing.T) {
+		request := newGetScoreRequest("Bob")
+		response := httptest.NewRecorder()
+
+		PlayerServer(response, request)
+
+		assertResponseBody(t, response.Body.String(), "10")
+	})
+}
+
+func newGetScoreRequest(name string) *http.Request {
+	request, _ := http.NewRequest("http.MethodGet", fmt.Sprintf("/players/%s", name), nil)
+	return request
+}
+
+func assertResponseBody(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("Response body is wrong, got: %q, but wanted: %q", got, want)
+	}
 }
